dba60e8dfeed0c377e71ec674734f52de2fef4f8
0736b <thanawat_bumpengpun@cmu.ac.th>
2023-01-16 05:41:51 +0700

    feat: img slider next, back without dot select

---
+++ resources/js/slide.js
@@ -0,0 +1,34 @@
+let slides = document.getElementsByClassName("slide");
+let btn_left = document.getElementById("btn-left");
+let btn_right = document.getElementById("btn-right");
+
+let prev = 0;
+let curr = 1;
+let next = 2;
+
+to_prev = () => {
+    curr > 0 ? to_curr(curr - 1) : to_curr(slides.length - 1);
+}
+
+to_next = () => {
+    curr < slides.length - 1 ? to_curr(curr + 1) : to_curr(0);
+}
+
+to_curr = (idx) => {
+    curr = idx;
+    prev = curr - 1;
+    next = curr + 1;
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('slide-prev');
+        slides[i].classList.remove('slide-curr');
+        slides[i].classList.remove('slide-next');
+    }
+    next >= slides.length ? next = 0 : next;
+    prev < 0 ? prev = slides.length - 1 : prev;
+    slides[prev].classList.add('slide-prev');
+    slides[curr].classList.add('slide-curr');
+    slides[next].classList.add('slide-next');
+}
+
+btn_left.addEventListener('click', to_prev);
+btn_right.addEventListener('click', to_next);
